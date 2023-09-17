58daba34fadb226e71827906dc314368d99760a0
rebecaCaroba <rebecacaroba@gmail.com>
2023-09-17 14:00:16 -0300

    Componente Container

--- src/components/Container/style.ts
+++ src/components/Container/style.ts
@@ -2,4 +2,10 @@ import styled from "styled-components";
 
 export const ContainerPass = styled.div`
     width: 500px;
+
+    h1 {
+        color: ${(props) => props.theme['gray-200']};
+        text-align: center;
+        margin: 2rem;
+    }
 `
