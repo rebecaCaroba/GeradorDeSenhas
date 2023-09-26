ff47176eea145f4adb47c2df94b6f472d0e3c908
rebecaCaroba <rebecacaroba@gmail.com>
2023-09-26 08:29:16 -0300

    Mudando a estrutura

--- src/components/Body/index.tsx
+++ src/components/Body/index.tsx
@@ -1,7 +1,8 @@
 import { Forca } from "../Força";
 import { AiOutlineArrowRight } from 'react-icons/ai'
-import { ContainerCaracter, ContainerForm, FormPass } from "./style";
+import { ContainerCaracter, ContainerForm, ContainerPass, FormPass } from "./style";
 import { useState } from "react";
+import { Header } from "../Header/Index";
 
 export function Body() {
     const [CharacterLength, setCharacterLength] = useState(15)
@@ -26,55 +27,58 @@ export function Body() {
     }
 
     return (
-        <ContainerForm>
-            <FormPass onSubmit={handleListPass}>
-                <ContainerCaracter>
-                    <p>Comprimento dos caracteres</p>
-                    <span>{CharacterLength}</span>
-                </ContainerCaracter>
-                <input type="range" id="strLength" onChange={handleCharacterLength}  min={5} max={25} />
-                <div>
-                    <input
-                    type="checkbox" 
-                    name="include-uppercase" 
-                    value="uppercase"
-                    onChange={handleCheckboxChange}
-                    />
-                    <label htmlFor="include-uppercase">Incluir letras maiúsculas</label>
-                </div>
-                <div>
-                    <input 
-                    type="checkbox" 
-                    name="include-lowercase" 
-                    value="lowercase"
-                    onChange={handleCheckboxChange}
-                    />
-                    <label htmlFor="include-lowercase">Incluir letras minúsculas</label>
-                </div>
-                <div>
-                    <input 
-                    type="checkbox" 
-                    name="include-numbers" 
-                    value="numbers"
-                    onChange={handleCheckboxChange}
-                    />
-                    <label htmlFor="include-numbers">Incluir numbers</label>
-                </div>
-                <div>
-                    <input 
-                    type="checkbox" 
-                    name="include-symbols" 
-                    value="symbols"
-                    onChange={handleCheckboxChange}
-                    />
-                    <label htmlFor="include-symbols">Incluir símbolos </label>
-                </div>
-                <Forca />
-                <button type="submit">
-                    GERAR
-                    <AiOutlineArrowRight size={14}/>
-                </button>
-            </FormPass>
-        </ContainerForm>
+        <ContainerPass>
+            <Header />
+            <ContainerForm>
+                <FormPass onSubmit={handleListPass}>
+                    <ContainerCaracter>
+                        <p>Comprimento dos caracteres</p>
+                        <span>{CharacterLength}</span>
+                    </ContainerCaracter>
+                    <input type="range" id="strLength" onChange={handleCharacterLength}  min={5} max={25} />
+                    <div>
+                        <input
+                        type="checkbox"
+                        name="include-uppercase"
+                        value="uppercase"
+                        onChange={handleCheckboxChange}
+                        />
+                        <label htmlFor="include-uppercase">Incluir letras maiúsculas</label>
+                    </div>
+                    <div>
+                        <input
+                        type="checkbox"
+                        name="include-lowercase"
+                        value="lowercase"
+                        onChange={handleCheckboxChange}
+                        />
+                        <label htmlFor="include-lowercase">Incluir letras minúsculas</label>
+                    </div>
+                    <div>
+                        <input
+                        type="checkbox"
+                        name="include-numbers"
+                        value="numbers"
+                        onChange={handleCheckboxChange}
+                        />
+                        <label htmlFor="include-numbers">Incluir numbers</label>
+                    </div>
+                    <div>
+                        <input
+                        type="checkbox"
+                        name="include-symbols"
+                        value="symbols"
+                        onChange={handleCheckboxChange}
+                        />
+                        <label htmlFor="include-symbols">Incluir símbolos </label>
+                    </div>
+                    <Forca />
+                    <button type="submit">
+                        GERAR
+                        <AiOutlineArrowRight size={14}/>
+                    </button>
+                </FormPass>
+            </ContainerForm>
+        </ContainerPass>
     )
 }
--- src/components/Body/style.ts
+++ src/components/Body/style.ts
@@ -1,5 +1,15 @@
 import styled from "styled-components";
 
+export const ContainerPass = styled.div`
+    width: 500px;
+    
+    h1 {
+        color: ${(props) => props.theme['gray-200']};
+        text-align: center;
+        margin: 2rem;
+    }
+`
+
 export const ContainerForm = styled.div`
     background-color: ${(props) => props.theme['gray-700']};
     width: 100%;
